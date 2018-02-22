37f7ec3098008b8978909042e1e7b6f49bc648ca
Henry Sanchez <hsanchez@healthesystems.com>
2018-02-22 12:02:13 -0500

    added quotes object.

--- randomquotes/js/main.js
+++ randomquotes/js/main.js
@@ -1,10 +1,46 @@
 var quotes = {
-    1 : "blah blah",
-    2 : "blajdkfj",
-    3 : "hello there",
-    4 : "blah adsf"
+    1 : [
+            "Life is so short that it must be lived very slowly.",
+            "Buddhist saying"
+        ],
+    2 : [
+            "Flow with whatever may happen and let your mind be free: Stay centered by accepting what you are doing. This is the ultimate.",
+            "Chuang-Tzu, The Writings of Chuang-Tzu"
+        ],
+    3 : [
+            "Self-control is strength; right thought is mastery; calmness is power; say into your heart, 'Peace, be still'",
+            "James Allen, As a Man Thinketh"
+        ],
+    4 : [
+            "Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.",
+            "Master Oogway"
+        ],
+    5 : [
+            "..the more you take, the less you have.",
+            "Master Oogway"
+        ],
+    6 : [
+            "There is just news. There is no good or bad.",
+            "Master Oogway"
+        ],
+    7 : [
+            "One often meets his destiny on the road he takes to avoid it.",
+            "Master Oogway"
+        ],
+    8 : [
+            "There are no accidents.",
+            "Master Oogway"
+        ],
+    9 : [
+            "Your mind is like this water, my friend. When it gets agitated, it becomes difficult to see. But if you allow it to settle, the answer becomes clear.",
+            "Master Oogway"
+        ],
+    10 : [
+            "There is always something more to learn. Even for a master.",
+            "Master Oogway"
+        ],
+    11 : [
+            "Self-control is strength; right thought is mastery; calmness is power; say into your heart, 'Peace, be still'",
+            "Master Oogway"
+        ],
 };
-
-// var quote = JSON.parse(quotes);
-
-// alert(quote.1);

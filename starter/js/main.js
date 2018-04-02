f411dbc7716286d94950d5c3ad33c50a00bdb346
Henry Sanchez <hsanchez@healthesystems.com>
2018-04-02 15:07:34 -0400

    removed onclick prop from element. added click effect for button.

--- starter/js/main.js
+++ starter/js/main.js
@@ -1,3 +1,8 @@
+var blueButton = document.querySelector('button');
+
+//click event
+blueButton.addEventListener('click', showAlert, false);
+
 function showAlert() {
     alert("Use these files to get started.. \n Now lets go! What are you waiting for?");
 }

5ad2ae55872d57ee9b795f842667592dca138d55
Henry Sanchez <hsanchez@healthesystems.com>
2018-04-03 16:43:13 -0400

    added show-menu class and transition.

--- nav/js/main.js
+++ nav/js/main.js
@@ -12,12 +12,20 @@ function spin(f) {
     if (f.target.className === 'drop-menu' && f.target.className !== 'sub-menu' && f.target.childNodes.length !== 0) {
 
         //console.log("clicked");
+
+        //get img node and toggle class
         f.target.children[0].classList.toggle('spin');
+
+        //grab sub-menu node and toggle class
+        f.target.childNodes[3].classList.toggle('show-menu');
 
     } else if (f.target.classList.contains('menu-icon')) {
         
         //console.log("clicked icon");
         f.target.classList.toggle('spin');
+
+        //grab sub-menu node and toggle class
+        f.target.nextElementSibling.classList.toggle('show-menu');
     }
     
     f.stopPropagation();

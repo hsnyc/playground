f36b4f5991b018fa861c877b5173911b12f4c2c6
Henry Sanchez <hsanchez@healthesystems.com>
2018-04-03 16:07:47 -0400

    added rotation for nav menu drop-down icon
    Removed ids (not needed). Cleaned up code.

--- nav/js/main.js
+++ nav/js/main.js
@@ -1,49 +1,24 @@
-// var blueButton = document.querySelector('.button');
-
+// NAV Menu Drop Downs
 
 var navMenu = document.querySelector('.nav-menu');
-console.log(navMenu);
+//console.log(navMenu);
 
-//var menuIcon = document.querySelector('.menu-icon');
-
-// dropClick(navMenu);
 navMenu.addEventListener('click', spin , false);
-
-// function dropClick(e) {
-    
-// }
-
-// for (var i = 0; i < navMenu.length; i++) {
-//     navMenu[i].addEventListener('click', spin , false);
-// }
-// navMenu.addEventListener('click', spin , false);
 
 //make drop icon turn 180deg
 function spin(f) {
-    console.log(f);
+    //console.log(f);
 
-    if (f.target !== f.currentTarget && f.target.childNodes.length !== 0) {
-        //var clickedMenu = document.querySelector('.' + f.target.className + ' img');
+    if (f.target.className === 'drop-menu' && f.target.className !== 'sub-menu' && f.target.childNodes.length !== 0) {
 
-        //console.log(clickedMenu);
-
+        //console.log("clicked");
         f.target.children[0].classList.toggle('spin');
 
-        //clickedMenu.classList.toggle('spin');
-        // clickedMenu.classList.toggle('spin');
-    } //else if (f.target.className === 'menu-icon' || f.target.className === 'menu-icon spin') {
-       // console.log(f.target.outerHTML);
-       // f.target.classList.toggle('spin');
-        //var clickedMenu = document.querySelector('.menu-icon');
-        //document.querySelector(f.path[1]);
-        //clickedMenu.classList.toggle('spin');
-        // var clickedMenu = document.querySelector('#' + f.target.id + ' img');
-        f.stopPropagation();
+    } else if (f.target.classList.contains('menu-icon')) {
+        
+        //console.log("clicked icon");
+        f.target.classList.toggle('spin');
+    }
+    
+    f.stopPropagation();
 }
- 
-//click event
-// blueButton.addEventListener('click', showAlert, false);
-
-// function showAlert() {
-//     alert("Use these files to get started.. \n Now lets go! What are you waiting for?");
-// }

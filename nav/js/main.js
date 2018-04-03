d52086b9ed8cf215905dced7afc697fa19eae2d9
Henry Sanchez <hsanchez@healthesystems.com>
2018-04-03 11:12:58 -0400

    adjusted drop down position and style.
    Added triangle and hover effect.

--- nav/js/main.js
+++ nav/js/main.js
@@ -1,8 +1,9 @@
-var blueButton = document.querySelector('button');
+var blueButton = document.querySelector('.button');
+
 
 //click event
-blueButton.addEventListener('click', showAlert, false);
+// blueButton.addEventListener('click', showAlert, false);
 
-function showAlert() {
-    alert("Use these files to get started.. \n Now lets go! What are you waiting for?");
-}
+// function showAlert() {
+//     alert("Use these files to get started.. \n Now lets go! What are you waiting for?");
+// }

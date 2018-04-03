061ada17ebaf711cf03c18e1758090d6da6861b2
Henry Sanchez <hsanchez@healthesystems.com>
2018-04-03 14:12:52 -0400

    icon spin toggle v.01

--- nav/js/main.js
+++ nav/js/main.js
@@ -1,6 +1,46 @@
-var blueButton = document.querySelector('.button');
+// var blueButton = document.querySelector('.button');
 
 
+var navMenu = document.querySelector('.nav-menu');
+console.log(navMenu);
+
+//var menuIcon = document.querySelector('.menu-icon');
+
+// dropClick(navMenu);
+navMenu.addEventListener('click', spin , false);
+
+// function dropClick(e) {
+    
+// }
+
+// for (var i = 0; i < navMenu.length; i++) {
+//     navMenu[i].addEventListener('click', spin , false);
+// }
+// navMenu.addEventListener('click', spin , false);
+
+//make drop icon turn 180deg
+function spin(f) {
+    console.log(f);
+
+    if (f.target !== f.currentTarget && f.target.childNodes.length !== 0) {
+        //var clickedMenu = document.querySelector('.' + f.target.className + ' img');
+
+        //console.log(clickedMenu);
+
+        f.target.children[0].classList.toggle('spin');
+
+        //clickedMenu.classList.toggle('spin');
+        // clickedMenu.classList.toggle('spin');
+    } //else if (f.target.className === 'menu-icon' || f.target.className === 'menu-icon spin') {
+       // console.log(f.target.outerHTML);
+       // f.target.classList.toggle('spin');
+        //var clickedMenu = document.querySelector('.menu-icon');
+        //document.querySelector(f.path[1]);
+        //clickedMenu.classList.toggle('spin');
+        // var clickedMenu = document.querySelector('#' + f.target.id + ' img');
+        f.stopPropagation();
+}
+ 
 //click event
 // blueButton.addEventListener('click', showAlert, false);
 

749cce46d1d25ad6a00a56d56d1a1f8c5b464fe3
Henry Sanchez <hsanchez@healthesystems.com>
2018-10-04 10:45:06 -0400

    some simple code for the main.js file.

--- components/js/main.js
+++ components/js/main.js
@@ -1,6 +1,18 @@
+
+//Insert name into heading
+var name = prompt("Hi. What is your name?", "Joe");
+
+var heading = document.querySelector('.main-title');
+if(name !== "null" && name) {
+   heading.innerHTML = "Hello " + name + "."; 
+} else {
+    heading.innerHTML = "Hello.";
+}
+
+//Console Hello 'name'
 var greeting = function hello(th) {
     // var th = 'the name';
-    console.log(th);
+    console.log('Hello ' + th);
 };
 
-greeting('Hello Henry');
+greeting(name);

e0dea415222d108b714aa8ac28d3eb3c00597203
Henry Sanchez <hsanchez@healthesystems.com>
2018-10-05 13:40:16 -0400

    filtering build v0.0.2

--- filtering/js/main.js
+++ filtering/js/main.js
@@ -7,15 +7,37 @@ var boxes = document.querySelectorAll('.box');
 var cat;
 for(let c of boxes) {
     cat = c.getAttribute('category');
-    console.log(cat);
+    // console.log(cat);
 }
-console.log(boxes);
+// console.log(boxes);
 
 //get nav links.
 var navLinks = document.querySelectorAll('.link');
-console.log(navLinks);
+// console.log(navLinks);
 
-//add click event for link. Get href attribute
+//add click event to each link to get attribute value.
+var l = 0;
+for(let nav of navLinks) {
+    nav.addEventListener("click",getLinkProp,false);
+    nav.linkID = l++; //to identify the link that was clicked on.
+}
+
+//filter boxes based on the link attribute that was clicked
+//TODO next..
+
+
+//<==== Functions ====>
+
+function getLinkProp(e) {
+    //grab the link-prop attribute 
+    let linkProp = e.target.getAttribute('link-prop');
+    console.log(linkProp);
+
+    return linkProp;
+}
+
+
+
 
 
 

27fb84481b622c2835170757ac2cf572f4d3cb1f
Henry Sanchez <hsanchez@healthesystems.com>
2018-10-09 09:10:08 -0400

    code cleanup.

--- filtering/js/main.js
+++ filtering/js/main.js
@@ -2,42 +2,22 @@
 
 //get items into an array
 var boxes = document.querySelectorAll('.box');
-// console.log(boxes);
-
-//set inline styles ::test
-// for(let b of boxes) {
-//     // b.style.opacity = '1';
-//     b.style.height = '200px';
-//     b.style.width = '200px';
-//     b.style.margin = '10px';
-// }
 
 //get categories attribute
 var catArr;
 for(let c of boxes) {
     catArr = c.getAttribute('category');
-    // console.log(catArr);
 }
-// console.log(boxes);
 
 //get nav links.
 var navLinks = document.querySelectorAll('.link');
-// console.log(navLinks);
 
 //add click event to each link to get attribute value.
 var l = 0;
 for(let nav of navLinks) {
     nav.addEventListener("click",filterBoxes,false);
-    nav.linkID = l++; //to identify the link that was clicked on.
+    //nav.linkID = l++; //to identify the link that was clicked on.
 }
-
-//filter boxes based on the link attribute that was clicked
-// let boxSet = boxes.filter((box) => {
-//     return linkProp == box.getAttribute('category');
-    
-// });
-
-
 
 
 //<==== Functions ====>
@@ -45,7 +25,7 @@ for(let nav of navLinks) {
 function filterBoxes(e) {
     //grab the link-prop attribute 
     let linkProp = e.target.getAttribute('link-prop');
-    console.log("Link Prop is: " + linkProp);
+    //console.log("Link Prop is: " + linkProp);
 
     //reset boxes
     for(let b of boxes) {
@@ -65,7 +45,7 @@ function filterBoxes(e) {
         b.style.opacity = '0.2';
     }
 
-    console.log(boxSet);
+    //console.log(boxSet);
 }
 
 

1d9b70bc6b9589833a51c1ef8a3d33084edd5372
Henry Sanchez <hsanchez@healthesystems.com>
2018-11-01 13:44:54 -0400

    Tabs build out. Major css modifications.

--- tabs/js/main.js
+++ tabs/js/main.js
@@ -4,51 +4,48 @@
 var tabs = document.querySelectorAll('.tab'); //one tab
 var tabPanels = document.querySelectorAll('.tab-panels'); //tab panels container
 var tabPanel = document.querySelectorAll('.tab-panel'); //single tab panel
+var firstPanel = document.querySelector('.first-panel'); //first tab panel
 var tabID;
 var idNum;
 var tabIndex;
 
 //IE bug: re-doing Loop
 for (var t = 0; t < tabs.length; t++) {
-    // console.log(tab);
     tabs[t].addEventListener('click', openTab, false);
 }
 
 //Open tab function
 function openTab(e) {
-    //console.log(e);
 
     //get ID of clicked tab
     tabID = e.target.id;
-    //console.log(tabID);
+    // console.log(tabID);
 
     //find out which panel should be reveal
     idNum = tabID.match(/\d/g); //returns array
-    idNum = idNum.join(''); 
-    tabIndex = idNum - 1;
-    //console.log(tabIndex);
+    idNum = idNum.join(''); //remove from array
+    tabIndex = idNum - 1; //subtract one due to zero based # in arrays
 
     //reveal panel relating to tab
-    if( tabPanel[tabIndex].style.display === 'flex') {
+    if(tabPanel[tabIndex].style.display === 'block') { //this can be Flex if needed
         
         //if panel is visible then hide it. 
         tabPanel[tabIndex].style.display = 'none';
-        // console.log(tabPanel[tabIndex]);
+
+        //and reveal first panel
+        firstPanel.style.display = 'flex';
 
     } else {
 
-        //else hide all visible panels
-        // for (let p of tabPanel) {
-        //     p.style.display = 'none';
-        // }
-
-        // EI11 bug - re-doing loop
         for (var p=0; p < tabPanel.length; p++) {
             tabPanel[p].style.display = 'none';
         }
 
+        //hide first panel
+        firstPanel.style.display = 'none'; //may not always need to do this but oh well.
+
         //and display currently clicked panel
-        tabPanel[tabIndex].style.display = 'flex';
+        tabPanel[tabIndex].style.display = 'block'; //this can be Flex if needed
     } 
 }
 

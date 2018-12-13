cd076b6f99e77646e08bcdb991f97c85f374ded2
Henry Sanchez <hsanchez@healthesystems.com>
2018-12-13 14:42:58 -0500

    edit tabs to remove the first panel

--- tabs/tab_v2/js/main.js
+++ tabs/tab_v2/js/main.js
@@ -4,7 +4,6 @@
 var tabs = document.querySelectorAll('.tab'); //one tab
 var tabPanels = document.querySelectorAll('.tab-panels'); //tab panels container
 var tabPanel = document.querySelectorAll('.tab-panel'); //single tab panel
-var firstPanel = document.querySelector('.first-panel'); //first tab panel
 var tabID;
 var idNum;
 var tabIndex;
@@ -26,27 +25,14 @@ function openTab(e) {
     idNum = idNum.join(''); //remove from array
     tabIndex = idNum - 1; //subtract one due to zero based # in arrays
 
-    //reveal panel relating to tab
-    if(tabPanel[tabIndex].style.display === 'block') { //this can be Flex if needed
-        
-        //if panel is visible then hide it. 
-        tabPanel[tabIndex].style.display = 'none';
+    //hide any visible panel
+    for (var p=0; p < tabPanel.length; p++) {
+        tabPanel[p].style.display = 'none';
+    }
 
-        //and reveal first panel
-        firstPanel.style.display = 'flex';
+    //and display currently clicked panel
+    tabPanel[tabIndex].style.display = 'block'; //this can be Flex if needed
 
-    } else {
-
-        for (var p=0; p < tabPanel.length; p++) {
-            tabPanel[p].style.display = 'none';
-        }
-
-        //hide first panel
-        firstPanel.style.display = 'none'; //may not always need to do this but oh well.
-
-        //and display currently clicked panel
-        tabPanel[tabIndex].style.display = 'block'; //this can be Flex if needed
-    } 
 }
 
 // ===== TABS End ======= //
